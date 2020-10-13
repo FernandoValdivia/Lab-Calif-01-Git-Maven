62829c01a4956d454ead2e0ad9a4caf5079d7974
excal <excal@astarte>
2020-10-13 17:26:20 -0500

    second commit

--- src/main/java/com/tecsup/lab01/view/UserView.java
+++ src/main/java/com/tecsup/lab01/view/UserView.java
@@ -1,5 +1,11 @@
 package com.tecsup.lab01.view;
+import com.tecsup.lab01.controller.*;
 
 public class UserView {
 
+	public static void main(String[] args){
+		
+		UserController usuario = UserController();
+		System.out.println(usuario.obtener());
+	}
 }
